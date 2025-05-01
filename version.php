f965ac5b4c4595c5d57755d8c0baa7c593d8a8f4
Paulo Júnior <pauloa.junior@ufla.br>
2025-05-01 19:56:53 -0300

    Fix bootstrap class for show quiz responses

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025042904;
+$plugin->version = 2025050100;
 $plugin->requires = 2020061500;
 $plugin->release = 'v.1.1.0';
 $plugin->component = 'block_smartedu';
