c6fe2c8c1d10dfd53a5d10d484f6698c7e638f2d
Paulo Júnior <pauloa.junior@ufla.br>
2025-05-22 20:47:35 -0300

    Fix chatpgpt prompt

--- amd/src/mindmap.js
+++ amd/src/mindmap.js
@@ -1,18 +1,16 @@
 define(['jquery', 'block_smartedu/mind-elixir'], function($, MindElixir) {
   return {
     init: function(mindMapData) {
-      let jsonData = mindMapData;
-      if (typeof mindMapData === 'string') {
-        jsonData = JSON.parse(mindMapData);
-      }
-        window.console.log(jsonData);
-        const mind = new MindElixir({
-            el: '#mindMap',
-            editable: false,
-            nodeMenu: false,
-            data: jsonData,
-        });
-        mind.init();
+      window.console.log(mindMapData);
+      mindMapData = JSON.parse(mindMapData);
+
+      const mind = new MindElixir({
+          el: '#mindMap',
+          editable: false,
+          nodeMenu: false,
+          data: mindMapData,
+      });
+      mind.init();
     }
   };
 });
